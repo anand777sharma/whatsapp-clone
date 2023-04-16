6fd3e573e31d1c8f9142f2ea5dc6a13f2c07cb47
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2023-04-16 18:57:42 +0530

    change

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import AccountProvider from './context/AccountProvider';
 
 function App() {
 
-  //  const clientId = process.env.GOOGLEID;
-   const clientId = '372256147435-hoku2hubpjlllis7nbj29or839jfc5bt.apps.googleusercontent.com';
+   const clientId = process.env.GOOGLEID;
+  //  const clientId = '372256147435-hoku2hubpjlllis7nbj29or839jfc5bt.apps.googleusercontent.com';
   
   return (
     <GoogleOAuthProvider clientId={clientId}>
